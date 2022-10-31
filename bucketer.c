5c85a3f6a0d96473cbbf08397db42c904b4e5b38
Raj Vir Singh <58128290+Raj-Vir@users.noreply.github.com>
2022-10-31 17:44:54 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -1,7 +1,20 @@
+//===========================================================================================================
+// Input: Number of test Cases t, size of array numberOfBatteries, enter the levels chargeCycleCounts
+// Output: level indication and count
+//===========================================================================================================
+// Sample testcases: 
+/*2-> number of test cases
+  4 -> size
+  50 -1 1100 500 -> array
+  3
+  200 600 100
+ */
+//===========================================================================================================
 #include <stdio.h>
+#include <stdlib.h> // for dynamic allocation of memory
 #include <assert.h>
 
-// structure definition consiting of 3 fields lowCount, mediumCount, highCount which will contain the count of each value
+// structure definition consiting of 3 fields lowCount, mediumCount, highCount
 struct CountsByUsage {
   int lowCount;
   int mediumCount;
@@ -9,10 +22,10 @@ struct CountsByUsage {
 };
 
 // funtion to increment the count by taking two inputs 
-// cycles array []={100, 300, 410, 500, 600, 900, 950, 1000, -10} 
-// nBatteries: number of batteries
+// cycles array-[]={100, 300, 500, 600, 900, 1000} 
+// nBatteries- number of batteries
 struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
-  // initializing the counts by 0, 0, 0
+  // counts contains the counting values of 3 fields in the structure
   struct CountsByUsage counts = {0, 0, 0};
 
 
@@ -43,13 +56,31 @@ struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
       printf("High\n");
     }
   }
-  // returning the count structure
   return counts;
 }
 
 void testBucketingByNumberOfCycles() {
-  const int chargeCycleCounts[] = {100, 300, 410, 500, 600, 900, 950, 1000, -10};
-  const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
+  int numberOfBatteries;
+  // allowing user to enter the number of battery levels
+  printf("Enter the number of battery levels you want to enter: ");
+  scanf("%d",&numberOfBatteries);
+
+  // dynamically allocation of array chargeCycleCounts
+  int *chargeCycleCounts;
+  chargeCycleCounts = (int *)malloc(numberOfBatteries*sizeof(int));
+  
+  // if memory is not allocated
+  if(chargeCycleCounts==NULL){
+    printf("Dynamic Allocation failed\n");
+    exit(0);
+  }
+
+
+  // taking values from user
+  printf("Enter the battery levels now: \n");
+  for(int i=0;i<numberOfBatteries;i++){
+    scanf("%d",&chargeCycleCounts[i]);
+  }
   printf("Counting batteries by usage cycles...\n");
 
   // count variable will contain all the battery count with different levels
@@ -68,11 +99,18 @@ void testBucketingByNumberOfCycles() {
   printf("Number of Low count batteries: %d\n",counts.lowCount);
   printf("Number of Medium count batteries: %d\n",counts.mediumCount);
   printf("Number of High count batteries: %d\n",counts.highCount);
+
+  // freeing the dynamically allocated values
+  free(chargeCycleCounts);
 }
 
 int main() {
-  // calling testBucketingByNumberOfCycles() from main()
-  testBucketingByNumberOfCycles();
-  // since main is returning nothing 
+  // number of test cases
+  int t;
+  printf("Enter the Number of test cases:\n");
+  scanf("%d",&t);
+  // looping till test cases are finished
+  while(t--)
+    testBucketingByNumberOfCycles();
   return 0;
 }
